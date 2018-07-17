d0315977f8e81233fabadee837b6ceb339133e99
Peter M C Harrison <pmc.harrison@gmail.com>
2018-07-17 10:28:41 +0100

    indenting

--- R/add-symbol.R
+++ R/add-symbol.R
@@ -2,9 +2,9 @@ add_symbol <- function(tree, value, save, when) {
   tree$active_order <- 0L
   for (j in seq_along(tree$active_nodes)) {
     res <- take_path(node = tree$active_nodes[[j]],
-                                        value = value,
-                                        save = save,
-                                        when = when)
+                     value = value,
+                     save = save,
+                     when = when)
     if (!is(res, "empty_node")) tree$active_order <- j
     tree$active_nodes[[j]] <- res
   }
