48cc44354db011c66827862571fc3ee39c3e5f0f
Peter M C Harrison <pmc.harrison@gmail.com>
2018-07-18 13:41:37 +0100

    Adding option for excluding terminals from get_num_children()

--- tests/testthat/test-get_num_children.R
+++ tests/testthat/test-get_num_children.R
@@ -2,9 +2,12 @@ context("get_num_children")
 
 test_that("example", {
   t <- new_tree()
-  add_seq(t, c("A", "A", "B", "C", "A", "B"),
-          terminate = FALSE)
-  expect_equal(get_num_children(t, 0), 3)
+  add_seq(t, c("A", "A", "B", "C", "A", "B"), terminate = TRUE)
+  # plot(t)
+  expect_equal(get_num_children(t, 0), 4)
+  expect_equal(get_num_children(t, 0, exclude_terminals = TRUE), 3)
+  t <- new_tree()
+  add_seq(t, c("A", "A", "B", "C", "A", "B"), terminate = FALSE)
   expect_equal(get_num_children(t, 1), 1)
   expect_equal(get_num_children(t, 2), 1)
   expect_equal(get_num_children(t, 3), 0)
